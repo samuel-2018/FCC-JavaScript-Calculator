aa1c020c7020860ad6dc7984d803b97575ae56f1
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-06-08 12:15:05 -0400

    added imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import KeyPad from './KeyPad.js';
+import MainDisplay from './MainDisplay.js';
+import getResult from './getResult';
 import './App.css'
 
 class App extends Component {
